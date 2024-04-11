ec7c93c2126c825f276b692f4dca95dddc505958
nathanso0 <nathan.so1165@gmail.com>
2024-04-11 14:27:09 -0400

    Its a start

--- Sketch.java
+++ Sketch.java
@@ -6,7 +6,9 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
+
     size(400, 400);
+
   }
 
   /** 
@@ -14,16 +16,25 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+
     background(210, 255, 173);
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    
-  }
 
+    for (int counter = 15; counter >= 0; counter -= 1){ 
+      for(int inty = 0; inty < height; inty +=height/10 ){
+        for (int intx = 0; intx < width; intx += width/10 ) {
+          house(intx, inty);
+        }
+      }
+    
+    }
+  }
   /**
    * Description
    * 
@@ -33,6 +44,12 @@ public class Sketch extends PApplet {
    * @author 
    */
 
+
+  private void house(int intHouseX, int intHouseY ){
+    rect(intHouseX, intHouseY, width/15, height/15);
+    rect(intHouseX+width/50, intHouseY+height/35, width/35, height/30);
+
+  }
   
   
 }
