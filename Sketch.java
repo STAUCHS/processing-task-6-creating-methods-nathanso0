04029c518fdac7b78d281f235a671dfaa3e7d0d7
nathanso0 <nathan.so1165@gmail.com>
2024-04-12 13:07:39 -0400

    unfinshedp2

--- Sketch.java
+++ Sketch.java
@@ -7,7 +7,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 
-    size(400, 400);
+    size(800, 800);
 
   }
 
@@ -27,12 +27,11 @@ public class Sketch extends PApplet {
   public void draw() {
 
     for (int counter = 15; counter >= 0; counter -= 1){ 
-      for(int inty = 0; inty < height; inty +=height/10 ){
-        for (int intx = 0; intx < width; intx += width/10 ) {
+      for(int inty = 0; inty < height; inty +=height/6 ){
+        for (int intx = 0; intx < width; intx += width/6 ) {
           house(intx, inty);
         }
       }
-    
     }
   }
   /**
@@ -46,9 +45,14 @@ public class Sketch extends PApplet {
 
 
   private void house(int intHouseX, int intHouseY ){
-    rect(intHouseX, intHouseY, width/15, height/15);
-    rect(intHouseX+width/50, intHouseY+height/35, width/35, height/30);
-
+    fill(100,25,0);
+    rect(intHouseX, intHouseY+height/20, width/15, height/15);
+    fill(150,75,0);
+    rect(intHouseX+width/50, intHouseY+height/15, width/35, height/20);
+    fill(35,35,35);
+    triangle(intHouseX+width/15, intHouseY+height/20, intHouseX+width/30, intHouseY+height/50, intHouseX, intHouseY+height/20);
+    fill(255,215,0);
+    ellipse(intHouseX+width/35, intHouseY+height/11, width/90, height/90);
   }
   
   
