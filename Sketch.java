f32c7fa236bf8c9703f34b1d12788eaa83b518b0
nathanso0 <nathan.so1165@gmail.com>
2024-04-15 14:29:13 -0400

    AlmostDone

--- Sketch.java
+++ Sketch.java
@@ -25,26 +25,86 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-
+    
     for (int counter = 15; counter >= 0; counter -= 1){ 
       for(int inty = 0; inty < height; inty +=height/6 ){
         for (int intx = 0; intx < width; intx += width/6 ) {
-          house(intx, inty);
+          drawhouse(intx, inty);
+        }
+      }
+    }
+    for (int counter3 = 15; counter3 >= 0; counter3 -= 1){ 
+      for(int inty3 = 0; inty3 < height; inty3 +=height/6){
+        for (int intx3 = 0; intx3 < width; intx3 += width/6 ) {
+          drawbody(intx3, inty3);
         }
       }
     }
   }
+/**
+   * Description
+   * 
+   * @param intHeadX
+   * @param intHeadY
+   * @return Boolean
+   * @Nathan.So 
+   */
+  public boolean drawhead(int intHeadX, int intHeadY){
+    fill(255,203,164);
+    ellipse(intHeadX+width/9, intHeadY+height/7, width/34, height/34);
+    if(intHeadX % 3 == 0 || intHeadY % 3 == 0){
+      return true;
+    }
+      else {
+        return false;
+      }
+
+    }
+      
+  public void drawbody(int intBodyX,int intBodyY){
+    boolean bllshirtcolor = drawhead(intBodyX, intBodyY);
+    if (bllshirtcolor == true){
+      fill(45,45,45);
+
+      rect(intBodyX+width/10, intBodyY+height/6, width/55, height/25);
+
+      rect(intBodyX+width/10, intBodyY+height/6, width/110, height/25);
+
+      fill(255,255,255);
+
+
+      rect(intBodyX+width/10, intBodyY+height/7, width/55, height/25);
+
+
+      drawhead(intBodyX, intBodyY);
+    }
+    else {
+
+      fill(0,0,200);
+
+      rect(intBodyX+width/10, intBodyY+height/6, width/55, height/25);
+
+      rect(intBodyX+width/10, intBodyY+height/6, width/110, height/25);
+
+      fill(0,0,0);
+      rect(intBodyX+width/10, intBodyY+height/7, width/55, height/25);
+
+      drawhead(intBodyX, intBodyY);
+    }
+  }
+
+
   /**
    * Description
    * 
-   * @param 
-   * @param 
-   * @return
-   * @author 
+   * @intHouseX 
+   * @intHouseY 
+   * @return Void
+   * @Nathan.So
    */
 
 
-  private void house(int intHouseX, int intHouseY ){
+  private void drawhouse(int intHouseX, int intHouseY ){
     fill(100,25,0);
     rect(intHouseX, intHouseY+height/20, width/15, height/15);
     fill(150,75,0);
